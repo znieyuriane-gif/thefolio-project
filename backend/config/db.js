96bf6c054a8633a058ade0251968aa512a63d58e
Stephanie Rivales <znieyuriane@gmail.com>
2026-04-08 09:38:10 +0800

    Fix database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,8 @@
-// backend/config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error("Database connection error:", err.message);

d1efe192450737e56f8115a41d879fd84e54fafe
Steph <znieyuriane@gmail.com>
2026-04-16 13:51:16 +0800

    Fixed the code

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true
 });
 
 // Attach token to every request automatically

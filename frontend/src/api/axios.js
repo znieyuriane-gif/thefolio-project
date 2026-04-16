5479d21005e0d6ddd707a43e8eba577ad62a406f
Stephanie Rivales <znieyuriane@gmail.com>
2026-04-16 23:40:27 +0800

    Fixed the code

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use environment variable for flexibility
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
 
